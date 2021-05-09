2d968f2901ad22f087ba8f5fb3fddd2eecc17754
Tsuyoshi CHO <Tsuyoshi.CHO@Gmail.com>
2021-05-09 17:31:45 +0900

    fix dup slash

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 # Check tag name conflicts
-vim --clean --not-a-term --cmd "try | helptags doc/ | catch | echo v:exception | cquit | endtry" --cmd quit
+vim --clean --not-a-term --cmd "try | helptags doc | catch | echo v:exception | cquit | endtry" --cmd quit
 
 # EOF
